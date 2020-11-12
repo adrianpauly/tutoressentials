e059634b5b5581b4a83831f508b964de2df8d6e2
Gabriel Villalobos <xnehilorx@gmail.com>
2020-11-11 22:59:18 -0600

    Restart quiz functionality

--- wp-content/themes/tutessentials/js/custom.js
+++ wp-content/themes/tutessentials/js/custom.js
@@ -1,30 +1,64 @@
 (function($) {
+    $(document).foundation();
+    $(document).ready(function() {
+        // Slick init        
+        $('.school-logos').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            speed: 700,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: false,
+            variableWidth: true
+        });
 
-	$(document).foundation()
+        $('.testimonials').slick({
+            infinite: true,
+            speed: 700,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            arrows: false
+        });
 
+        var $startQuizBtn = $('.wpProQuiz_content').find('[name="startQuiz"]');
+        if ($startQuizBtn.length) {
+            $('.wpProQuiz_content').find('[name="startQuiz"]').on('click', function() {
+                watchResults();
+            });
 
-    $(document).ready(function(){
+            function watchResults() {
+                var observer = new MutationObserver(function(mutations) {
+                    mutations.forEach(function(mutationRecord) {
+                        checkResults();
+                    });
+                });
+                var target = document.getElementsByClassName('wpProQuiz_results');
+                observer.observe(target[0], {
+                    attributes: true,
+                    attributeFilter: ['style']
+                });
+            }
 
-		// Slick init        
-		$('.school-logos').slick({
-		  infinite: true,
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
-		  speed: 700,
-		  autoplay: true,
-		  autoplaySpeed: 1500,
-		  arrows: false,
-		  variableWidth: true
-		});   
+            function checkResults() {
+                var $pointsContainer = $('.wpProQuiz_points');
+                var $pointsMarkers = $($pointsContainer).find('span');
+                var currentScore = $($pointsMarkers[0]).text();
+                var totalQuestions = $($pointsMarkers[1]).text();
+                if (currentScore != totalQuestions) {
+                    var $container = $('.wpProQuiz_results');
+                    var btnTemplate = "<input class='wpProQuiz_button wpProQuiz_button_restartQuiz' type='button' name='restartQuiz' value='Restart Quiz'>";
+                    $($container).find('.ld-quiz-actions').append(btnTemplate);
+                    $('.wpProQuiz_button_restartQuiz').on('click', resetQuiz);
+                    var messageTemplate = "<div class='alert alert-danger'>You have to answer all questions correctly in order to go to the next module.</div>";
+                    $($container).append(messageTemplate);
+                }
+            }
 
-		$('.testimonials').slick({
-		  infinite: true,
-		  speed: 700,
-		  autoplay: true,
-		  autoplaySpeed: 4000,
-		  arrows: false
-		});
-
+            function resetQuiz() {
+                window.location.reload(true);
+            }
+        }
     });
 
 
@@ -33,8 +67,8 @@
 
 
     // Expandable text
-    $('a[data-toggle=more-info]').on('click',function() {
-    	$('#more-info').slideToggle();
+    $('a[data-toggle=more-info]').on('click', function() {
+        $('#more-info').slideToggle();
     });
 
 
@@ -45,61 +79,51 @@
 
     if ($('.min-word-count-value').length) {
         min = $('.min-word-count-value').text();
-    } 
+    }
 
     console.log('min', min);
 
-    if(min && min > 0) {
+    if (min && min > 0) {
 
         $field.after('<div class="character-count"><span>0</span> / ' + min + ' minimum word count</div>')
 
-    	$('[name="startQuiz"]').on('click',function(){
-    		//$('.wpProQuiz_button').addClass('disabled');
-    	});
+        $field.on('keyup', function(e) {
 
-        $field.on('keyup',function(e){
+            // Count words
+            var wordSplit = $(this).val().split(/\s+/);
+            var wordCount = 0;
 
-        	// Count words
-        	var wordSplit = $(this).val().split(/\s+/);
-        	var wordCount = 0;
+            // but skip spaces
+            wordSplit.forEach((el) => {
+                if (el !== "") wordCount++;
+            });
 
-        	// but skip spaces
-        	wordSplit.forEach((el)=>{
-        		if (el !== "") wordCount++;
-        	});
+            $('.character-count span').text(wordCount);
 
-        	$('.character-count span').text(wordCount);
-
-        	if(wordCount >= min) {
-        		$('.character-count').addClass('good');
-    			$('.wpProQuiz_button').removeClass('disabled');
-        	} else {
-    			$('.wpProQuiz_button').addClass('disabled');
-        		$('.character-count').removeClass('good');
-        	}
+            if (wordCount >= min) {
+                $('.character-count').addClass('good');
+                $('.wpProQuiz_button').removeClass('disabled');
+            } else {
+                $('.wpProQuiz_button').addClass('disabled');
+                $('.character-count').removeClass('good');
+            }
 
         });
 
     }
 
-
-
     // Hack to remove Mark Complete button 
-
-    if ( $('#sfwd-mark-complete').length && $('.next-link').length ) {
+    if ($('#sfwd-mark-complete').length && $('.next-link').length) {
         $('.next-link').remove();
         $('#learndash_mark_complete_button').addClass('button-to-link').attr('value', 'Next Lesson →');
-    }
-
-
+	}
+	
     // Hack to hide completed quiz questions -- sorry, Learndash is finnicky ¯\_(ツ)_/¯ 
-    if ( $('.wpProQuiz_lock').text().indexOf('You passed this quiz') > -1 ) {
+    if ($('.wpProQuiz_lock').text().indexOf('You passed this quiz') > -1) {
         $('.wpProQuiz_list').hide();
-        setTimeout(function(){
+        setTimeout(function() {
             $('.wpProQuiz_text').show();
-        },1000)
-    }
-
-
-
-})(jQuery); 
+        }, 1000)
+	}
+	
+})(jQuery);
